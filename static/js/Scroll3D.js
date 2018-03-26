16b97a40bfbabea12a9e4d781f4c1c3870e206e8
Nick Jensen <nickthejensen@gmail.com>
2018-03-26 01:16:13 -0600

    Improved scrolling.

--- static/js/Scroll3D.js
+++ static/js/Scroll3D.js
@@ -7,6 +7,9 @@ class Scroll3D extends Component {
   constructor(props) {
     super(props);
     this._itemsToDom = this._itemsToDom.bind(this);
+    this.mouseDown = this.mouseDown.bind(this);
+    this.mouseUp = this.mouseUp.bind(this);
+    this.mouseMove = this.mouseMove.bind(this);
     const total = props.items.length;
     const step = 2 / total;
     let progress = 0;
@@ -22,17 +25,8 @@ class Scroll3D extends Component {
         progress = -1 + (progress - 1);
       }
     }
-    console.log(this.items)
     this.imgWidthPercent = 70;
-  }
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-  handleScroll(event) {
-    console.log(event);
+    this.state.scrolling = false;
   }
   _getMinAbsoluteProgress(items) {
     let least = { progress: 1 };
@@ -48,8 +42,12 @@ class Scroll3D extends Component {
     return maxOffset * Math.sin(progress * Math.PI);
   }
   _progressToHeight(progress) {
-    const minHeight = 40;
-    return (100 - minHeight) * Math.cos((1-Math.abs(progress)) * Math.PI) + minHeight;
+    const minHeight = 30;
+    // return (100 - minHeight) * Math.cos((1-Math.abs(progress)) * Math.PI) + minHeight;
+    if (progress < -0.5 || progress > 0.5) {
+      return minHeight;
+    }
+    return 100;
   }
   _itemsToDom(props) {
     return this.items.map(item =>{
@@ -75,6 +73,59 @@ class Scroll3D extends Component {
       )
     });
   }
+  _updateProgress(diff) {
+    this.items = this.items.map(item=>{
+      item.progress = item.progress + (diff / 100);
+      if (item.progress > 1) {
+        item.progress = -1 + (item.progress - 1);
+      }
+      if (item.progress < -1) {
+        item.progress = 1 + (item.progress + 1);
+      }
+      return item;
+    })
+  }
+  _evenOut(amount) {
+    this.items = this.items.map(item=>{
+      item.progress = item.progress + amount;
+      if (item.progress > 1) {
+        item.progress = -1 + (item.progress - 1);
+      }
+      if (item.progress < -1) {
+        item.progress = 1 + (item.progress + 1);
+      }
+      return item;
+    })
+  }
+  // TODO handle tap (go one to the left, or one to the right.)
+  mouseDown(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({scrolling: true, prevX: e.clientX });
+  }
+  // TODO this aught to animate back to nearest item. It isn't animating right now.
+  mouseUp(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({scrolling: false});
+    let closest = this._getMinAbsoluteProgress(this.items);
+    const rate = 500;
+    const amount = -1 * (closest.progress / rate);
+    for (let i = rate; i > 0; i--) {
+      this._evenOut(amount);
+      this.setState({remaining: i})
+    }
+  }
+  mouseMove(e, scrolling, prevX) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!scrolling) {
+      return;
+    }
+    const diff = e.clientX - prevX;
+    this._updateProgress(diff);
+    this.setState({prevX: e.clientX});
+  }
   render(props, state) {
     let itemRight = this._getMinAbsoluteProgress(this.items.filter(x => x.progress <= 0));
     let itemLeft = this._getMinAbsoluteProgress(this.items.filter(x => x.progress >= 0));
@@ -82,12 +133,21 @@ class Scroll3D extends Component {
     if (itemRight == itemLeft) {
       TITLE = itemRight.title;
     }
+    let leftOpacity = 0;
+    let rightOpacity = 0;
+    if (Math.abs(itemRight.progress) < Math.abs(itemLeft.progress)) {
+      rightOpacity = 1 - Math.abs(itemRight.progress);
+      leftOpacity = 1 - rightOpacity;
+    } else {
+      leftOpacity = 1 - Math.abs(itemLeft.progress);
+      rightOpacity = 1 - leftOpacity;
+    }
     return (
-      h('div', { className: "Scroll3D" },
+      h('div', { className: "Scroll3D", onMouseDown:this.mouseDown, onMouseUp:this.mouseUp, onMouseMove:(e)=>this.mouseMove(e, state.scrolling, state.prevX) },
         this._itemsToDom(),
         TITLE ? h('span', { className: 'title', style: 'opacity: 1;' }, TITLE)
-          : h('span', { className: 'title', style: `opacity: ${Math.abs(1 - itemLeft.progress)};` }, itemLeft.title),
-        TITLE ? null : h('span', { className: 'title', style: `opacity: ${Math.abs(1 - itemRight.progress)};` }, itemRight.title)
+          : h('span', { className: 'title', style: `opacity: ${leftOpacity};` }, itemLeft.title),
+        TITLE ? null : h('span', { className: 'title', style: `opacity: ${rightOpacity};` }, itemRight.title)
       )
     )
   }
--- static/js/personalScrollItems.js
+++ static/js/personalScrollItems.js
@@ -11,18 +11,5 @@ const personalScrollItems = [
     {
         title: 'Chainmail Artist',
         img: '/static/img/dragonscaleSquare.png'
-    },
-{
-        title: 'Developer',
-        img: '/static/img/python.svg'
-    },
-    {
-        title: 'Researcher',
-        img: '/static/img/beaker.png'
-        //img: '/static/img/DNA-Helix-Variation-2.svg'
-    },
-    {
-        title: 'Chainmail Artist',
-        img: '/static/img/dragonscaleSquare.png'
-    },
+    }
 ]
